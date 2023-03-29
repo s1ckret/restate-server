ac8271ceac15cf068b25f9c901f219c1e970004f
Ivan Kovalenko <s1ckretbox@gmail.com>
2023-03-29 13:24:09 +0300

    feat: create project
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ restate-server-core-1/Program.cs
@@ -0,0 +1,37 @@
+using restate_server_core_1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<adsContext>();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("*");
+        });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
