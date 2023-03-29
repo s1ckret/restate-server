abae90e960193313c626377999878648c9b7490f
Ivan Kovalenko <s1ckretbox@gmail.com>
2023-03-29 13:25:14 +0300

    feat: add Ads controllers
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ restate-server-core-1/Controllers/AdsController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restate_server_core_1.Models;
+
+namespace restate_server_core_1.Controllers
+{
+    [Route("api/ads")]
+    [ApiController]
+    public class AdsController : ControllerBase
+    {
+        private readonly adsContext _context;
+
+        public AdsController(adsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int page = 1, int limit = 2, int buildingId = 0)
+        {
+            if (page < 1)
+            {
+                return BadRequest("Page parameter should be positive!");
+            }
+
+            if (limit <= 0 || limit > 20)
+            {
+                return BadRequest("Limit parameter should be in range[1, 20]");
+            }
+
+            page -= 1;
+
+            if (buildingId <= 0)
+            {
+                int firstId = 1 + page * limit;
+                var range = Enumerable.Range(firstId, limit);
+
+                var postings = _context.Ads
+                    .Where(it => range.Contains(it.Id))
+                    .Select(it => new
+                    {
+                        building = new
+                        {
+                            id = it.Building.Id,
+                            street = it.Building.Street,
+                            houseNumber = it.Building.HouseNumber,
+                            maxFloors = it.Building.MaxFloors,
+                            lat = it.Building.Lat,
+                            lon = it.Building.Lon
+                        },
+                        photo = it.Photos.ToList().Select(p => p.Key),
+                        postedAtSec = it.PostedAtSec,
+                        foundAtSec = it.FoundAtSec,
+                        atFloor = it.AtFloor,
+                        title = it.Title,
+                        description = it.Description,
+                        price = it.Price,
+                        currency = it.Currency,
+                        readyForLiving = it.ReadyForLiving,
+                        roomQty = it.RoomQty
+
+                    });
+
+                return Ok(postings);
+            } else
+            {
+                var postings = _context.Ads
+                    .Where(it => it.BuildingId == buildingId)
+                    .Select(it => new
+                    {
+                        building = new
+                        {
+                            id = it.Building.Id,
+                            street = it.Building.Street,
+                            houseNumber = it.Building.HouseNumber,
+                            maxFloors = it.Building.MaxFloors,
+                            lat = it.Building.Lat,
+                            lon = it.Building.Lon
+                        },
+                        photo = it.Photos.ToList().Select(p => p.Key),
+                        postedAtSec = it.PostedAtSec,
+                        foundAtSec = it.FoundAtSec,
+                        atFloor = it.AtFloor,
+                        title = it.Title,
+                        description = it.Description,
+                        price = it.Price,
+                        currency = it.Currency,
+                        readyForLiving = it.ReadyForLiving,
+                        roomQty = it.RoomQty
+
+                    })
+                    .Skip(page * limit)
+                    .Take(limit);
+
+                return Ok(postings);
+            }
+            
+        }
+
+    }
+}
---
+++ restate-server-core-1/Controllers/AdsPagesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restate_server_core_1.Models;
+
+namespace restate_server_core_1.Controllers
+{
+    [Route("api/ads/pages")]
+    [ApiController]
+    public class AdsPagesController : ControllerBase
+    {
+        private readonly adsContext _context;
+
+        public AdsPagesController(adsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int size = 2, int buildingId = 0)
+        {
+
+            int ads = 0;
+            if (buildingId <= 0)
+            {
+                ads = _context.Ads.Count();
+            }
+            else
+            {
+                ads = _context.Ads.Where(it => it.BuildingId == buildingId).Count();
+            }
+            int pages = ads / size;
+            return Ok(pages);
+        }
+    }
+}
