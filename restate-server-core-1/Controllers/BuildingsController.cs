48327be16b06b7aed87fe1d9ec8ba3f206bd135a
Ivan Kovalenko <s1ckretbox@gmail.com>
2023-03-29 13:24:57 +0300

    feat: add BuildingController
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ restate-server-core-1/Controllers/BuildingsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restate_server_core_1.Models;
+
+namespace restate_server_core_1.Controllers
+{
+    [Route("api/buildings/geojson")]
+    [ApiController]
+    public class BuildingsController : ControllerBase
+    {
+        private readonly adsContext _context;
+
+        public BuildingsController(adsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var buildings = _context.Buildings
+            .Select(b => new { Id = b.Id, Geojson = b.Geojson })
+            .ToList();
+
+            return Ok(buildings);
+        }
+    }
+}
